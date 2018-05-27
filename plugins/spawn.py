ceab02ec47259073cbe68e7fc90a17dcf6d9b324
David Rosa Tamsen <rosatamsen@gmail.com>
2018-05-26 23:26:52 -0700

    gzdev: Implemented core + plugins prototype with spawn command.

--- plugins/spawn.py
+++ plugins/spawn.py
@@ -4,149 +4,59 @@ Usage:
 	            [<ros> | --ros=<distro_name>]
 	            [<config> | --config=<file_name>]
 	            [<pr> | --pr=<number>]
-	            [--dev | --source]
 	            [--yes]
 	gzdev spawn -h | --help
 	gzdev spawn --version
 
 Options:
-	-h --help               Show this screen
-	--version               Show gzdev's version
-	--gzv=<number>          Gazebo release version number
-	--ros=<distro_name>     ROS distribution name
-	--config=<file_name>    World configuration file
-	--pr=<number>           Branch to compile from based on Pull Request #
-	--dev                   Install Gazebo development libraries
-	--source                Build Gazebo/ROS from source
-	--yes                   Confirm selection of unofficial ROS + Gazebo version
+	-h --help               Show this screen.
+	--version               Show gzdev's version.
+	--gzv=<number>          Gazebo version number.
+	--ros=<distro_name>     ROS distribution name.
+	--config=<file_name>    Gazebo world configuration file.
+	--pr=<number>           Gazebo branch to compile from based on Pull Request #.
+	--yes                   Confirm selection of unofficial ROS + Gazebo version.
 
 """
 from docopt import docopt
-from sys import stderr
-from subprocess import run as run_cmd
-from subprocess import PIPE, CompletedProcess
-from os import environ
-from dockerfile_parse import DockerfileParser
-from pprint import pprint
-import json
-import docker
 
-official_ros_gzv = {"kinetic": 7, "lunar": 7, "melodic": 9}
-gz7_ros = {}.fromkeys(["kinetic", "lunar"])
-gz8_ros = {}.fromkeys(["kinetic", "lunar"])
-gz9_ros = {}.fromkeys(["kinetic", "lunar", "melodic"])
-compatible = {7: gz7_ros, 8: gz8_ros, 9: gz9_ros}
-max_gzv = 9
+args = docopt(__doc__, version="gzdev-spawn 0.1")
 
+gzv = args["<gzv>"] if args["<gzv>"] else args["--gzv"]
+ros = args["<ros>"] if args["<ros>"] else args["--ros"]
+config = args["<config>"] if args["<config>"] else args["--config"]
+pr = args["<pr>"] if args["<pr>"] else args["--pr"]
+confirm = args["--yes"]
 
-def normalize_args(args):
-	gzv = args["<gzv>"] if args["<gzv>"] else args["--gzv"]
-	ros = args["<ros>"] if args["<ros>"] else args["--ros"]
-	config = args["<config>"] if args["<config>"] else args["--config"]
-	pr = args["<pr>"] if args["<pr>"] else args["--pr"]
-	confirm = args["--yes"]
+gz_msg, ros_msg, config_msg, pr_msg = ("", "", "", "")
 
-	ros = ros.lower() if ros else None
-	gzv = int(gzv) if gzv and gzv.isdecimal() else gzv
-	if gzv == None and ros and ros in official_ros_gzv:
-		gzv = official_ros_gzv[ros]
+if (ros):
+	ros_msg = " + ROS %s" % ros
+	if gzv == None or (gzv and not confirm):
+		tmp = gzv
 
-	return gzv, ros, config, pr, confirm
+		if ros == "melodic": gzv = "9"
+		if ros == "lunar": gzv = "7"
+		if ros == "kinetic": gzv = "7"
+		if ros == "jade": gzv = "5"
+		if ros == "indigo": gzv = "2"
 
+		if tmp != None and tmp != gzv:
+			print("\nUnofficial Gazebo %s%s version selected!" % (tmp, ros_msg),
+			"If you know what you are doing add option --y to confirm selection and continue.\n",
+			"*Otherwise, please visit http://gazebosim.org/tutorials?tut=ros_wrapper_versions for more info.\n")
+			exit()
 
-def error(msg):
-	print("\n" + msg + "\n", file=stderr)
-	exit(0)
+if (gzv):
+	gz_msg = "\nSpawning docker container for Gazebo %s" % gzv
 
+if (config):
+	config_msg = " running world configuration %s" % config
 
-def validate_input(args):
-	gzv, ros, config, pr, confirm = args
+if (pr):
+	pr_msg = " from PR #%s" % pr
 
-	if type(gzv) is int and (gzv <= 0 or gzv > max_gzv) or type(gzv) is str:
-		error("ERROR: '%s' is not a valid Gazebo version number." % gzv)
-
-	if gzv and gzv not in compatible:
-		error("ERROR: This tool does not support Gazebo %d." % gzv)
-
-	if ros and ros not in official_ros_gzv:
-		error("ERROR: '%s' is not a valid/supported ROS distribution." % ros)
-
-	if gzv and ros and ros not in compatible[gzv]:
-		error("ERROR: Gazebo %d is not compatible with ROS %s!" % (gzv, ros))
-
-	tmp = official_ros_gzv[ros] if ros else None
-	if ros and tmp != None and tmp != gzv and not confirm:
-		error("WARNING: Unofficial Gazebo %d + ROS %s version selected!\n" %
-			(gzv, ros) + "We recommend using Gazebo %d + ROS %s :)\n\n" %
-			(tmp, ros) + "    * If you know what you are doing, " +
-			"then add option --y to confirm selection and continue.\n" +
-			"    * Otherwise, please visit"
-			"http://gazebosim.org/tutorials?tut=ros_wrapper_versions"
-			"for more info.")
-
-
-def run(args):
-	gzv, ros, config, pr, confirm = args
-	gz_msg, ros_msg, config_msg, pr_msg = ("", "", "", "")
-
-	if ros:
-		ros_msg = " + ROS %s" % ros
-
-	if gzv:
-		gz_msg = "Spawning docker container for Gazebo %d" % gzv
-	else:
-		error("ERROR: Gazebo version was not specified.")
-
-	if config:
-		config_msg = " running world configuration %s" % config
-
-	if pr:
-		pr_msg = " from PR# %s" % pr
-
-	print("\n" + gz_msg + ros_msg + config_msg + pr_msg + "...\n")
-	if __name__ == '__main__':
-		docker_run(str(gzv))
-
-
-def docker_run(gzv):
-	docker_build = docker.from_env().images.build
-	docker_run = docker.from_env().containers.run
-	runtime = "nvidia"
-
-	try:
-		run_cmd(["nvidia-docker", "version"])
-	except FileNotFoundError:
-		runtime = ""
-
-	docker_build(path="docker", rm=True, buildargs={"GZV": gzv}, tag="gz" + gzv)
-
-	docker_run(
-		"gz" + gzv,
-		stdin_open=True,
-		tty=True,
-		detach=True,
-		environment=["DISPLAY=" + environ["DISPLAY"], "QT_X11_NO_MITSHM=1"],
-		ports={'10000': 10000},
-		volumes={'/tmp/.X11-unix':{'bind':'/tmp/.X11-unix', 'mode':'rw'}},
-		name="gz" + gzv,
-		remove=True,
-		runtime=runtime)
-
-	try:
-		ret = run_cmd(["xpra", "attach", "tcp:localhost:10000"],
-						stdout=PIPE, stderr=PIPE, universal_newlines=True)
-	except KeyboardInterrupt:
-		exit()
-
-	with open("gz" + gzv + ".log", 'w') as log_file:
-		log_file.write(ret.stdout)
-
-
-def main():
-	args = normalize_args(docopt(__doc__, version="gzdev-spawn 0.1.0"))
-	validate_input(args)
-	run(args)
-
-
-if __name__ == '__main__':
-	main()
+if not gzv:
+	print(__doc__)
+else:
+	print(gz_msg + ros_msg + config_msg + pr_msg + ".\n")
