515995b0c10c4f9e4202811d6f1e46035ca0d012
David Rosa <rosatamsen@gmail.com>
2018-06-07 19:24:43 -0700

    [hotfix] Add zmq and uuid to apt-get install list.

--- docker/setup.sh
+++ docker/setup.sh
@@ -4,12 +4,9 @@ apt-get install -y ruby-dev ruby-ronn wget lsb-release mercurial vim python3 pyt
 #echo "deb-src http://packages.osrfoundation.org/gazebo/$ID-stable `lsb_release -sc` main" > /etc/apt/sources.list.d/gazebo-latest.list
 # wget http://packages.osrfoundation.org/gazebo.key -O - | apt-key add
 apt-add-repository -y ppa:dartsim
-apt-get update && apt-get install -y libtinyxml2-dev libdart6-dev libfreeimage-dev libprotoc-dev libprotobuf-dev protobuf-compiler freeglut3-dev libcurl4-openssl-dev libtinyxml-dev libtar-dev libtbb-dev libogre-1.9-dev libxml2-dev pkg-config qtbase5-dev libqwt-qt5-dev libltdl-dev libgts-dev libboost-thread-dev libboost-signals-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-regex-dev libboost-iostreams-dev libbullet-dev libsimbody-dev
-
+apt-get update && apt-get install -y libzmq3-dev uuid-dev libtinyxml2-dev libdart6-dev libfreeimage-dev libprotoc-dev libprotobuf-dev protobuf-compiler freeglut3-dev libcurl4-openssl-dev libtinyxml-dev libtar-dev libtbb-dev libogre-1.9-dev libxml2-dev pkg-config qtbase5-dev libqwt-qt5-dev libltdl-dev libgts-dev libboost-thread-dev libboost-signals-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-regex-dev libboost-iostreams-dev libbullet-dev libsimbody-dev
 # echo "deb http://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/ ./" >> /etc/apt/sources.list
 # wget https://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/Release.key -O - | apt-key add
-apt-get install libzmq3-dev uuid-dev
-
 pip3 install --upgrade pip wheel setuptools colcon-common-extensions vcstool
 mkdir -p /tmp/gazebo/src && cd /tmp/gazebo
 wget https://gist.githubusercontent.com/dirk-thomas/6c1ca2a7f5f8c70ce7d3e1ef10a9f678/raw/490aaba72321284af956c9db12f9ef1550ef88cf/Gazebo9.repos
