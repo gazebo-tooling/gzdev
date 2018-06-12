3dfd2d657777f8440dfcd509ce5e893aae3c8fc5
David Rosa <rosatamsen@gmail.com>
2018-06-12 00:00:49 -0700

    [refactor] Add run function to make testing easier.

--- plugins/spawn.py
+++ plugins/spawn.py
@@ -19,15 +19,15 @@ Options:
 
 """
 from docopt import docopt
-from sys import stderr
 
-ros_gzv = {"melodic":9, "lunar":7, "kinetic":7, "indigo":2}
+ros_gzv = {"melodic": 9, "lunar": 7, "kinetic": 7, "indigo": 2}
 gz2 = {}.fromkeys(["indigo"])
 gz7 = {}.fromkeys(["indigo", "kinetic", "lunar"])
 gz8 = {}.fromkeys(["kinetic", "lunar"])
 gz9 = {}.fromkeys(["kinetic", "lunar", "melodic"])
 
 compatible = [None, None, gz2, None, None, None, None, gz7, gz8, gz9]
+
 
 def parse_args():
 	args = docopt(__doc__, version="gzdev-spawn 0.1")
@@ -43,56 +43,68 @@ def parse_args():
 
 	return gzv, ros, config, pr, confirm
 
-def error(msg):
-	print("\n" + msg + "\n", file=stderr)
-	exit()
 
-def validate_input(gzv, ros):
-	if type(gzv) is int and (gzv <= 0 or gzv >= len(compatible)) or type(gzv) is str:
+def error(msg):
+	# raise SystemExit("\n" + msg + "\n")
+	exit("\n" + msg + "\n")
+
+
+def validate_input():
+	gzv, ros, config, pr, confirm = argv
+
+	if type(gzv) is int and (gzv <= 0 or
+		gzv >= len(compatible)) or type(gzv) is str:
 		error("ERROR: '%s' is not a valid Gazebo version number." % gzv)
 
-	if not gzv and ros not in ros_gzv:
+	if not gzv and ros and ros not in ros_gzv:
 		error("ERROR: '%s' is not a valid/supported ROS distribution." % ros)
 
 	if gzv and not compatible[gzv]:
 		error("ERROR: This tool does not support Gazebo %d." % gzv)
 
-	if gzv and ros not in compatible[gzv]:
+	if gzv and ros and ros not in compatible[gzv]:
 		error("ERROR: Gazebo %d is not compatible with ROS %s!" % (gzv, ros))
 
-gzv, ros, config, pr, confirm = parse_args()
-validate_input(gzv, ros)
-gz_msg, ros_msg, config_msg, pr_msg = ("", "", "", "")
 
-if (ros):
-	ros_msg = " + ROS %s" % ros
+def run():
+	gzv, ros, config, pr, confirm = argv
+	gz_msg, ros_msg, config_msg, pr_msg = ("", "", "", "")
 
-	if not gzv or (gzv and not confirm):
-		tmp = gzv
-		gzv = ros_gzv[ros]
+	if ros:
+		ros_msg = " + ROS %s" % ros
 
-		if tmp != None and tmp != gzv:
-			print(
-				"WARNING: Unofficial Gazebo %d%s version selected!\n" %
-				(tmp, ros_msg),
-				"We recommend using Gazebo %d%s :)\n\n" % (gzv, ros_msg),
-				"    * If you know what you are doing, ",
-				"then add option --y to confirm selection and continue.\n",
-				"    * Otherwise, please visit"
-				"http://gazebosim.org/tutorials?tut=ros_wrapper_versions"
-				"for more info.\n", sep="")
-			exit()
+		if gzv == None or (gzv and not confirm):
+			tmp = gzv
+			gzv = ros_gzv[ros]
 
-if (gzv):
-	gz_msg = "\nSpawning docker container for Gazebo %d" % gzv
-else:
-	print(__doc__)
-	exit()
+			if tmp != None and tmp != gzv:
+				print(
+					"WARNING: Unofficial Gazebo %d%s version selected!\n" %
+					(tmp, ros_msg),
+					"We recommend using Gazebo %d%s :)\n\n" % (gzv, ros_msg),
+					"    * If you know what you are doing, ",
+					"then add option --y to confirm selection and continue.\n",
+					"    * Otherwise, please visit"
+					"http://gazebosim.org/tutorials?tut=ros_wrapper_versions"
+					"for more info.\n",
+					sep="")
+				exit()
 
-if (config):
-	config_msg = " running world configuration %s" % config
+	if gzv:
+		gz_msg = "\nSpawning docker container for Gazebo %d" % gzv
+	else:
+		exit("\nERROR: Gazebo version was not specified.\n")
 
-if (pr):
-	pr_msg = " from PR# %s" % pr
+	if config:
+		config_msg = " running world configuration %s" % config
 
-print(gz_msg + ros_msg + config_msg + pr_msg + ".\n")
+	if pr:
+		pr_msg = " from PR# %s" % pr
+
+	print(gz_msg + ros_msg + config_msg + pr_msg + ".\n")
+
+
+argv = parse_args()
+if __name__ == '__main__':
+	validate_input()
+	run()
