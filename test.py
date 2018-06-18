d1eb93d2ac97bae25cf29661312138006fa6ba9d
David Rosa <rosatamsen@gmail.com>
2018-06-18 14:58:26 -0700

    [testing] Add tests for all valid Gazebo/ROS inputs.

--- test.py
+++ test.py
@@ -1,49 +1,102 @@
 from pytest import raises
 from plugins import spawn
 
+argv = {
+	"<gzv>": None,
+	"<ros>": None,
+	"<config>": None,
+	"<pr>": None,
+	"--gzv": None,
+	"--ros": None,
+	"--config": None,
+	"--pr": None,
+	"--yes": None
+}
 
-def test_input():
-	spawn.validate_input((7, None, None, None, None))
-	spawn.validate_input((8, None, None, None, None))
-	spawn.validate_input((9, None, None, None, None))
-	spawn.validate_input((7, "kinetic", None, None, None))
-	spawn.validate_input((7, "lunar", None, None, None))
-	spawn.validate_input((9, "melodic", None, None, None))
-	with raises(SystemExit):
-		spawn.validate_input((0, "melodic", None, None, None))
-
-
-def test_output(capsys):
-	spawn.run((9, "melodic", None, None, None))
-	captured = capsys.readouterr()
-	assert captured.out == "\nSpawning docker container for Gazebo 9 + ROS melodic.\n\n"
-
+run_code = {}
+error_code = {}
 
 def execute(argv):
-	args = spawn.parse_args(argv)
+	args = spawn.normalize_args(argv)
 	spawn.validate_input(args)
 	spawn.run(args)
+	reset_argv()
 
 
-def test_all():
-	argv = {
-		"<gzv>": None,
-		"<ros>": None,
-		"<config>": None,
-		"<pr>": None,
-		"--gzv": None,
-		"--ros": None,
-		"--config": None,
-		"--pr": None,
-		"--yes": False
-	}
+def reset_argv():
+	for k in argv:
+		argv[k] = None
+
+
+# def check_run_code(capsys):
+# 	spawn.run((9, "melodic", None, None, None))
+# 	captured = capsys.readouterr()
+# 	assert captured.out == "\nSpawning docker container for Gazebo 9 + ROS melodic.\n\n"
+
+
+def test_gzv():
+	gzv = ("7", "8", "9")
+	bad_gzv = ("0", "6", "10", "a")
+
+	for v in gzv:
+		argv["--gzv"] = v
+		execute(argv)
+
 	with raises(SystemExit):
 		execute(argv)
-		argv["<gzv>"] = "0"
-		execute(argv)
-	argv["<gzv>"] = "9"
-	execute(argv)
-	argv["<gzv>"] = "8"
-	execute(argv)
+		for v in bad_gzv:
+			argv["--gzv"] = v
+			execute(argv)
 
-test_all()
+
+def test_gz_ros():
+	gzv = ("7", "07", "009")
+	ros = ("Kinetic", "LUNAR", "melodic")
+
+	for i in range(len(gzv)):
+		argv["--gzv"] = gzv[i]
+		argv["--ros"] = ros[i]
+		execute(argv)
+
+	for i in range(len(ros)):
+		argv["--ros"] = ros[i]
+		execute(argv)
+
+	gzv = ("7", "8")
+	with raises(SystemExit):
+		for i in range(len(gzv)):
+			argv["--gzv"] = gzv[i]
+			argv["--ros"] = "melodic"
+			execute(argv)
+
+	ros = ("Indigo", "jade", "turtle", "!@#$")
+	with raises(SystemExit):
+		for i in range(len(ros)):
+			argv["--ros"] = ros[i]
+			execute(argv)
+
+
+def test_gz_ros_unofficial():
+	gzv = ("8", "8", "9", "9")
+	ros = ("kinetic", "lunar", "kinetic", "lunar")
+
+	with raises(SystemExit):
+		for i in range(len(gzv)):
+			argv["--gzv"] = gzv[i]
+			argv["--ros"] = ros[i]
+			execute(argv)
+
+	for i in range(len(gzv)):
+		argv["--yes"] = True
+		argv["--gzv"] = gzv[i]
+		argv["--ros"] = ros[i]
+		execute(argv)
+
+
+def run_all():
+	test_gzv()
+	test_gz_ros()
+	test_gz_ros_unofficial()
+
+
+run_all()
