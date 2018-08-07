726002ec59d0cc1552f626dca40505e155c2792f
David Rosa <rosatamsen@gmail.com>
2018-08-07 01:17:10 -0700

    [scripts] Rename to gzrepos.sh gzxpra.sh and add gzxvfb.sh

--- docker/gzsrc/gzrepos.sh
+++ docker/gzsrc/gzrepos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir src &&
+mkdir src
 curl https://bitbucket.org/rosatamsen/gazebodistro/raw/default/gazebo${GZV}.yml -o Gazebo${GZV}.repos &&
 vcs import src < Gazebo${GZV}.repos &&
 colcon metadata add default https://raw.githubusercontent.com/colcon/colcon-metadata-repository/master/index.yaml &&
---
+++ docker/gzsrc/gzxvfb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xvfb-run -s "-screen 0 1280x1024x24" gazebo --verbose
